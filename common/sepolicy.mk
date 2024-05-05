5eb0edd680b8ce3963e8f3b6888ced4e1d1a23e4
RenzAlt <129452476+shenprjkt@users.noreply.github.com>
2024-05-05 03:46:17 +0000

    sepolicy: Adapt for Weeb Project

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -16,20 +16,20 @@ endif
 endif
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/miku/sepolicy/common/public
+    device/weeb/sepolicy/common/public
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/miku/sepolicy/common/private
+    device/weeb/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/miku/sepolicy/common/dynamic \
-    device/miku/sepolicy/common/system
+    device/weeb/sepolicy/common/dynamic \
+    device/weeb/sepolicy/common/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/miku/sepolicy/common/dynamic \
-    device/miku/sepolicy/common/vendor
+    device/weeb/sepolicy/common/dynamic \
+    device/weeb/sepolicy/common/vendor
 endif
 
 # Selectively include legacy rules defined by the products
--include device/miku/sepolicy/legacy-common/sepolicy.mk
+-include device/weeb/sepolicy/legacy-common/sepolicy.mk
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -10,16 +10,16 @@ endif
 endif
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/miku/sepolicy/qcom/private
+    device/weeb/sepolicy/qcom/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/miku/sepolicy/qcom/dynamic \
-    device/miku/sepolicy/qcom/system
+    device/weeb/sepolicy/qcom/dynamic \
+    device/weeb/sepolicy/qcom/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/miku/sepolicy/qcom/dynamic \
-    device/miku/sepolicy/qcom/vendor
+    device/weeb/sepolicy/qcom/dynamic \
+    device/weeb/sepolicy/qcom/vendor
 endif
 
 ifeq (,$(filter msm8960 msm8226 msm8610 msm8974 apq8084 msm8909 msm8916 msm8952 msm8992 msm8994 msm8937 msm8953 msm8996 msm8998 sdm660 sdm710 sdm845, $(TARGET_BOARD_PLATFORM)))
